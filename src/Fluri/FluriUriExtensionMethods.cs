d5c786358df30685614d30e07d40526e80a3a9e8
Wouter Huysentruit <wouter.huysentruit@cognistreamer.com>
2018-01-22 11:13:40 +0100

    Initial commit

---
+++ src/Fluri/FluriUriExtensionMethods.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+// ReSharper disable once CheckNamespace
+namespace System
+{
+    public static class FluriUriExtensionMethods
+    {
+        private static readonly string FakeSchemeAndServer = $"fake://{Guid.NewGuid():N}";
+        private static readonly ConcurrentDictionary<Type, PropertyInfo[]> PropertyCache = new ConcurrentDictionary<Type, PropertyInfo[]>();
+
+        public static Uri WithQueryParameter(this Uri uri, string parameterName) => uri.WithQueryParameter(parameterName, null);
+
+        public static Uri WithQueryParameter(this Uri uri, string parameterName, string parameterValue)
+        {
+            if (parameterName == null) throw new ArgumentNullException(nameof(parameterName));
+            return uri.CombineQueryParameters((Name: parameterName, Value: parameterValue));
+        }
+
+        public static Uri WithQueryParameters(this Uri uri, IReadOnlyDictionary<string, string> parameters)
+        {
+            if (parameters == null) throw new ArgumentNullException(nameof(parameters));
+            return uri.CombineQueryParameters(parameters.Select(x => (Name: x.Key, Value: x.Value)).ToArray());
+        }
+
+        public static Uri WithQueryParameters(this Uri uri, object parametersObject, bool ignoreNullValues = false)
+        {
+            if (parametersObject == null) throw new ArgumentNullException(nameof(parametersObject));
+
+            var parameterProperties = PropertyCache.GetOrAdd(
+                parametersObject.GetType(),
+                type => type.GetProperties(BindingFlags.Instance | BindingFlags.GetProperty).ToArray());
+
+            var parameters = parameterProperties.Select(property => (Name: property.Name, Value: property.GetValue(parametersObject)?.ToString()));
+            if (ignoreNullValues) parameters = parameters.Where(x => x.Value != null);
+            return uri.CombineQueryParameters(parameters.ToArray());
+        }
+
+        private static Uri CombineQueryParameters(this Uri uri, params (string Name, string Value)[] parameters)
+        {
+            var absoluteUri = uri.IsAbsoluteUri ? uri : new Uri(new Uri(FakeSchemeAndServer), uri);
+
+            var uriWithoutQueryAndFragment = absoluteUri.GetComponents(UriComponents.SchemeAndServer | UriComponents.Path, UriFormat.Unescaped);
+
+            var builder = new UriBuilder(uriWithoutQueryAndFragment)
+            {
+                Query = absoluteUri.GetCombinedQueryComponent(parameters),
+                Fragment = absoluteUri.GetComponents(UriComponents.Fragment, UriFormat.Unescaped)
+            };
+
+            if (uri.IsAbsoluteUri) return builder.Uri;
+
+            var pattern = $"^{FakeSchemeAndServer}{(uri.OriginalString[0] != '/' ? "/" : string.Empty)}";
+            var relativeUri = Regex.Replace(builder.Uri.AbsoluteUri, pattern, string.Empty);
+            return new Uri(relativeUri, UriKind.Relative);
+        }
+
+        private static string GetCombinedQueryComponent(this Uri uri, params (string Name, string Value)[] parameters)
+        {
+            var builder = new StringBuilder(uri.GetComponents(UriComponents.Query, UriFormat.UriEscaped));
+            foreach (var parameter in parameters)
+            {
+                if (builder.Length > 0) builder.Append("&");
+                builder.Append(Uri.EscapeUriString(parameter.Name));
+                if (parameter.Value == null) continue;
+                builder.Append("=");
+                builder.Append(Uri.EscapeUriString(parameter.Value));
+            }
+
+            return builder.ToString();
+        }
+
+        public static Uri WithFragment(this Uri uri, string fragment)
+        {
+            if (string.IsNullOrEmpty(fragment)) throw new ArgumentNullException(nameof(fragment));
+
+            var absoluteUri = uri.IsAbsoluteUri ? uri : new Uri(new Uri(FakeSchemeAndServer), uri);
+
+            var uriWithoutFragment = absoluteUri.GetComponents(UriComponents.SchemeAndServer | UriComponents.PathAndQuery, UriFormat.Unescaped);
+
+            var builder = new UriBuilder(uriWithoutFragment)
+            {
+                Fragment = Uri.EscapeUriString(fragment)
+            };
+
+            if (uri.IsAbsoluteUri) return builder.Uri;
+
+            var pattern = $"^{FakeSchemeAndServer}{(uri.OriginalString[0] != '/' ? "/" : string.Empty)}";
+            var relativeUri = Regex.Replace(builder.Uri.AbsoluteUri, pattern, string.Empty);
+            return new Uri(relativeUri, UriKind.Relative);
+        }
+    }
+}
---
+++ tests/Fluri.Tests/FluriUriExtensionMethodsTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Fluri.Tests
+{
+    [TestFixture]
+    public class FluriUriExtensionMethodsTests
+    {
+        [Test]
+        public void WithQueryParameter_AbsoluteUriWithSchemeAndServer_ShouldAddFirstQueryParameter()
+        {
+            var original = new Uri("http://fluri.org:1234");
+            Assert.That(original.WithQueryParameter("abc").ToString(), Is.EqualTo("http://fluri.org:1234/?abc"));
+            Assert.That(original.WithQueryParameter("abc", "true").ToString(), Is.EqualTo("http://fluri.org:1234/?abc=true"));
+        }
+
+        [Test]
+        public void WithQueryParameter_AbsoluteUriWithSchemeServerAndFragment_ShouldAddFirstQueryParameter()
+        {
+            var original = new Uri("http://fluri.org:1234#fragment");
+            Assert.That(original.WithQueryParameter("abc").ToString(), Is.EqualTo("http://fluri.org:1234/?abc#fragment"));
+            Assert.That(original.WithQueryParameter("abc", "true").ToString(), Is.EqualTo("http://fluri.org:1234/?abc=true#fragment"));
+        }
+
+        [Test]
+        public void WithQueryParameter_AbsoluteUriWithSchemeServerPathAndFragment_ShouldAddFirstQueryParameter()
+        {
+            var original = new Uri("http://fluri.org:1234/some/path#fragment");
+            Assert.That(original.WithQueryParameter("abc").ToString(), Is.EqualTo("http://fluri.org:1234/some/path?abc#fragment"));
+            Assert.That(original.WithQueryParameter("abc", "true").ToString(), Is.EqualTo("http://fluri.org:1234/some/path?abc=true#fragment"));
+        }
+
+        [Test]
+        public void WithQueryParameter_AbsoluteUriWithEscapedCharactersInQueryString_ShouldKeepEscapedCharacters()
+        {
+            var original = new Uri("https://fluri.org/my/endpoint?v%20a=1%202%203");
+            Assert.That(original.WithQueryParameter("a b").AbsoluteUri, Is.EqualTo("https://fluri.org/my/endpoint?v%20a=1%202%203&a%20b"));
+            Assert.That(original.WithQueryParameter("a b", "12 3").AbsoluteUri, Is.EqualTo("https://fluri.org/my/endpoint?v%20a=1%202%203&a%20b=12%203"));
+        }
+
+        [Test]
+        public void WithQueryParameter_RelativeUri_ShouldAddFirstQueryParameter()
+        {
+            var original = new Uri("/test/endpoint", UriKind.Relative);
+            Assert.That(original.WithQueryParameter("abc").ToString(), Is.EqualTo("/test/endpoint?abc"));
+            Assert.That(original.WithQueryParameter("abc", "123").ToString(), Is.EqualTo("/test/endpoint?abc=123"));
+        }
+
+        [Test]
+        public void WithQueryParameter_RelativeUriWithQuery_ShouldAddSecondParameter()
+        {
+            var original = new Uri("/test/endpoint?initial=true", UriKind.Relative);
+            Assert.That(original.WithQueryParameter("abc").ToString(), Is.EqualTo("/test/endpoint?initial=true&abc"));
+            Assert.That(original.WithQueryParameter("abc", "123").ToString(), Is.EqualTo("/test/endpoint?initial=true&abc=123"));
+        }
+
+        [Test]
+        public void WithQueryParameter_RelativeUriWithQueryAndFragment_ShouldAddSecondParameter()
+        {
+            var original = new Uri("/test/endpoint?initial=true#frag", UriKind.Relative);
+            Assert.That(original.WithQueryParameter("abc").ToString(), Is.EqualTo("/test/endpoint?initial=true&abc#frag"));
+            Assert.That(original.WithQueryParameter("abc", "123").ToString(), Is.EqualTo("/test/endpoint?initial=true&abc=123#frag"));
+        }
+
+        [Test]
+        public void WithQueryParameter_RelativeUriWithoutLeadingSlash_ShouldAddFirstQueryParameterWithoutAddingLeadingSlash()
+        {
+            var original = new Uri("test/endpoint", UriKind.Relative);
+            Assert.That(original.WithQueryParameter("abc", "123").ToString(), Is.EqualTo("test/endpoint?abc=123"));
+        }
+
+        [Test]
+        public void WithFragment_AbsoluteUriWithoutFragment_ShouldAddFragment()
+        {
+            var original = new Uri("https://fluri.org:4433/test?initial=true");
+            Assert.That(original.WithFragment("Some Fragment").AbsoluteUri, Is.EqualTo("https://fluri.org:4433/test?initial=true#Some%20Fragment"));
+        }
+
+        [Test]
+        public void WithFragment_AbsoluteUriWithFragment_ShouldReplaceFragment()
+        {
+            var original = new Uri("https://fluri.org:4433/test?initial=true#myfragment");
+            Assert.That(original.WithFragment("Some Fragment").AbsoluteUri, Is.EqualTo("https://fluri.org:4433/test?initial=true#Some%20Fragment"));
+        }
+
+        [Test]
+        public void WithFragment_RelativeUriWithoutFragment_ShouldAddFragment()
+        {
+            var original = new Uri("/test?initial=true", UriKind.Relative);
+            Assert.That(original.WithFragment("newfragment").ToString(), Is.EqualTo("/test?initial=true#newfragment"));
+        }
+
+        [Test]
+        public void WithFragment_RelativeUriWithFragment_ShouldReplaceFragment()
+        {
+            var original = new Uri("/test?initial=true#myfragment", UriKind.Relative);
+            Assert.That(original.WithFragment("newfragment").ToString(), Is.EqualTo("/test?initial=true#newfragment"));
+        }
+    }
+}
